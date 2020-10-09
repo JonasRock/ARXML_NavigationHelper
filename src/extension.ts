b8ff22da447bf4182d7b2bc01bd7597df42c915b
Jonas Rock <jonasrock@gmx.de>
2020-10-09 12:12:17 +0200

    added hardcoded exectuion of the server mock

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,38 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as path from 'path';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+import { workspace,
+	ExtensionContext,
+	Extension,
+	window
+} from 'vscode';
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "arxml-navigationhelper" is now active!');
+import { LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient';
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('arxml-navigationhelper.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+import * as child_process from 'child_process';
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ARXML_NavigationHelper!');
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+	let currentPath: string;
+	const relativeLocationOfServerExecutable: string = '../../ARXML_LanguageServer/build/ARXML_LanguageServer.exe';
+	currentPath = __dirname;
+	currentPath = path.join(currentPath, relativeLocationOfServerExecutable);
+	window.showInformationMessage(currentPath);
+	let execOptions: Object;
+	execOptions = {
+		shell: true,
+		windowsHide: false
+	};
+	const child = child_process.execFile(currentPath, execOptions, (error, stdout, stderr) => {
+		if (error) {
+			throw error;
+		}
+		console.log(stdout);
 	});
-
-	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
