7f8f83b3ef7e04ebe593959444ceb8ecd2df7edf
Jonas Rock <jonasrock@gmx.de>
2020-10-13 10:45:43 +0200

    rewritten server to use sockets

--- src/extension.ts
+++ src/extension.ts
@@ -1,37 +1,45 @@
-import * as path from 'path';
-
-import { workspace,
-	ExtensionContext,
-	Extension,
-	window
-} from 'vscode';
-
-import { LanguageClient,
-	LanguageClientOptions,
-	ServerOptions,
-	TransportKind
-} from 'vscode-languageclient';
-
+import * as net from 'net';
+import { SocketMessageReader, SocketMessageWriter, Trace } from 'vscode-jsonrpc';
+import { window, workspace, commands, ExtensionContext, Uri, languages } from 'vscode';
+import { LanguageClient, LanguageClientOptions, StreamInfo, Position as LSPosition, Location as LSLocation, ServerOptions} from 'vscode-languageclient';
+import { lchmod, ReadStream } from 'fs';
+import { Socket } from 'dgram';
 import * as child_process from 'child_process';
 
-let client: LanguageClient;
+let socket: net.Socket;
 
 export function activate(context: ExtensionContext) {
-	let currentPath: string;
-	const relativeLocationOfServerExecutable: string = '../../ARXML_LanguageServer/build/ARXML_LanguageServer.exe';
-	currentPath = __dirname;
-	currentPath = path.join(currentPath, relativeLocationOfServerExecutable);
-	window.showInformationMessage(currentPath);
-	let execOptions: Object;
-	execOptions = {
-		shell: true,
-		windowsHide: false
+
+	let arxmlLSPath: string = ""; //Path to LanguageServer Executable
+
+	return launchServer(context, arxmlLSPath);
+}
+
+function launchServer(context: ExtensionContext, serverPath: string)
+{
+	const serverOptions: ServerOptions = () => createServerWithSocket(serverPath).then<StreamInfo>(() => ({ reader: socket, writer: socket }));
+	const clientOptions: LanguageClientOptions = {
+		documentSelector: [{ language: 'xml' }]
 	};
-	const child = child_process.execFile(currentPath, execOptions, (error, stdout, stderr) => {
-		if (error) {
-			throw error;
-		}
-		console.log(stdout);
+	const client = new LanguageClient('ARXML_LanguageServer', 'ARXML Language Server', serverOptions, clientOptions);
+
+	context.subscriptions.push(client.start());
+}
+
+function createServerWithSocket(executablePath: string)
+{
+	let exec: child_process.ChildProcess;
+	return new Promise<child_process.ChildProcess>(resolve => {
+		let server = net.createServer(s => {
+			socket = s;
+			socket.setNoDelay(true);
+			server.close();
+			resolve(exec);
+		});
+
+		server.listen(12730, '127.0.0.1', () => {
+			exec = child_process.spawn(executablePath);
+		});
 	});
 }
 
